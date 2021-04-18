33247b91179a5290e9697a1f5b009f496a95cebe
TanerTkn <73550556+TanerTkn@users.noreply.github.com>
2021-04-18 05:42:36 +0300

    color constant

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -5,7 +5,6 @@ class ColorConstants {
 
   ColorConstants._init();
 
-  final primaryColor = Color(0xFFEF6C00);
   final perfume = Color(0xFFC2A1F6);
   final purpleHeart = Color(0xFF5b13d1);
   final portage = Color(0xFFB18AF4);
