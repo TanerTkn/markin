c171a791ce9eae674125d97a1fc212ab6ebbf0e9
TanerTkn <73550556+TanerTkn@users.noreply.github.com>
2021-04-18 05:40:46 +0300

    vote widget ok

--- lib/widgets/vote_widget.dart
+++ lib/widgets/vote_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:markin/constant/color_constant.dart';
 import 'package:markin/core/extension/context_extension.dart';
 import 'package:markin/models/campaign_model.dart';
+import 'package:markin/view/detail/detail_view.dart';
 
 class VoteWidget extends StatelessWidget {
   final Campaign data;
@@ -26,71 +27,73 @@ class VoteWidget extends StatelessWidget {
           children: [
             Container(
               height: context.sizeH(0.12),
-              width: context.sizeH(0.4),
+              // width: context.sizeH(0.4),
+              width: double.infinity,
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: Colors.grey[100],
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(data.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18)),
-                      SizedBox(height: context.sizeH(0.01)),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Row(
-                          children: [
-                            SvgPicture.asset(
-                              "assets/svgs/vote.svg",
-                              height: 15,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            RichText(
-                                text: TextSpan(children: [
-                              TextSpan(
-                                text: data.markCount.toString(),
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.teal,
-                                    fontSize: 13),
-                              ),
-                              TextSpan(
-                                text: "  Voted",
-                                style:
-                                    TextStyle(color: Colors.grey, fontSize: 13),
-                              ),
-                            ])),
-                          ],
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Finished MarkIns June 15 2021", style: TextStyle(color: ColorConstants.instance.purpleHeart, fontWeight: FontWeight.bold)),
+                    SizedBox(height: context.sizeH(0.01)),
+                    Text(data.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                    SizedBox(height: context.sizeH(0.01)),
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          "assets/svgs/vote.svg",
+                          height: 15,
                         ),
-                      )
-                    ],
-                  ),
-                  CupertinoButton(
-                    child: Container(
-                      height: context.sizeH(0.1),
-                      width: context.sizeW(0.15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: ColorConstants.instance.electricViolet,
-                      ),
-                      child: Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white,
-                      ),
-                    ),
-                    onPressed: () {},
-                  )
-                ],
+                        SizedBox(
+                          width: 10,
+                        ),
+                        RichText(
+                            text: TextSpan(children: [
+                          TextSpan(
+                            text: data.markCount.toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.teal, fontSize: 13),
+                          ),
+                          TextSpan(
+                            text: "  Voted",
+                            style: TextStyle(color: Colors.grey, fontSize: 13),
+                          ),
+                        ])),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: CupertinoButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailView(),
+                  ));
+                },
+                child: Container(
+                  height: context.sizeH(0.05),
+                  width: context.sizeW(0.30),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Voted Now",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConstants.instance.electricViolet,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      )),
+                ),
+              ),
+            )
           ],
         ),
       ),
