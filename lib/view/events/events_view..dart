cb8cd6400edde165b724574737f9964b20170a7b
TanerTkn <73550556+TanerTkn@users.noreply.github.com>
2021-04-18 05:43:52 +0300

    events

---
+++ lib/view/events/events_view..dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventsView extends StatefulWidget {
+  @override
+  _EventsViewState createState() => _EventsViewState();
+}
+
+class _EventsViewState extends State<EventsView> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
